ecc187f809352e2a99b663e1d49f2d6df31c5e64
Friedrich Eichenroth <friedrich.eichenroth@gmail.com>
2024-01-16 17:58:35 +0100

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('GPT with Context extension activated');
+
+	context.subscriptions.push(
+		vscode.window.createTreeView('GPTWithContext_MainView', {
+			treeDataProvider: {
+				getTreeItem: (element: vscode.TreeItem) => element,
+				getChildren: (element?: vscode.TreeItem) => {
+					if (element) { return []; }
+					return [
+						new vscode.TreeItem('Tree item 1'),
+						new vscode.TreeItem('Tree item 2'),
+					];
+				},
+			},
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('gpt-with-context.testCommand', () => {
+			vscode.window.showInformationMessage('testcommand');
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('gpt-with-context.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from GPT with Context!');
+		})
+	);
+}
+
+export function deactivate() {}
